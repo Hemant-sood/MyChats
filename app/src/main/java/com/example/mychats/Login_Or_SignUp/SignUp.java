abbd2fa28ef944807397f66a160a7ac4e89c07e3
Hemant-sood <hemantsood1999@gmail.com>
2020-10-07 21:40:37 +0530

    Now proper way of reading and writing in My Account setting

--- app/src/main/java/com/example/mychats/Login_Or_SignUp/SignUp.java
+++ app/src/main/java/com/example/mychats/Login_Or_SignUp/SignUp.java
@@ -8,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,10 +22,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.jar.Attributes;
 
 public class SignUp extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference mRef;
     private EditText signupPassword, signupEmail, signupName;
     private Button  signUpButton;
     private String email, password, name;
@@ -45,14 +53,19 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View view) {
                 getDataFromEditText();  // get data from input fields
                 if( !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(name) ) {
+
+
+
                     mProgressDialog.setTitle("Sign Up progress..");
                     mProgressDialog.setMessage("One moment please...");
                     mProgressDialog.setCanceledOnTouchOutside(false);
                     mProgressDialog.show();
+
                     signUp(view);
-                    Intent main = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(main);
-                    finish();
+
+//                    Intent main = new Intent(getApplicationContext(), MainActivity.class);
+//                    startActivity(main);
+//                    finish();
                 }
                 else{
                     Snackbar.make(view, "All fields are mandatory", Snackbar.LENGTH_LONG).show();
@@ -72,7 +85,6 @@ public class SignUp extends AppCompatActivity {
 
 
     public void init(){
-        mAuth = FirebaseAuth.getInstance();
         signupEmail = findViewById(R.id.signup_email_id);
         signupPassword = findViewById(R.id.signup_password);
         signupName = findViewById(R.id.signup_name);
@@ -93,11 +105,7 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    mProgressDialog.dismiss();
-                    Toast.makeText(getApplicationContext(), "Account created Success",Toast.LENGTH_SHORT).show();
-                    Intent main = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(main);
-                    finish();
+                    createUserInRealTimeDB();
                 }
                 else{
                     mProgressDialog.hide();
@@ -111,5 +119,43 @@ public class SignUp extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void createUserInRealTimeDB() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = currentUser.getUid();
+
+        Log.d("User id ", uid);
+
+        mRef = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+        HashMap<String, String> map= new HashMap<>();
+
+        map.put("Name", name);
+        map.put("Status", "");
+        map.put("MyImg", "Link");
+
+        mRef.setValue(map)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Account created Success",Toast.LENGTH_SHORT).show();
+                            mProgressDialog.dismiss();
+                            Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(loginActivity);
+                            finish();
+                        }
+                        else{
+                            task.addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    }
+                });
+
     }
 }
